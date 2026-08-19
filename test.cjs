ef281d282c2cb4ce047dca758d1fa89f8f73e741
Sony23fd <snnbyrdv@gmail.com>
2026-08-19 22:40:31 +0800

    svvlinx

--- test.cjs
+++
@@ -1,5 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-prisma.user.findMany().then(u => {
-  console.log(u.map(x => ({ email: x.email, role: x.role, isActive: x.isActive })));
-}).catch(console.error).finally(() => prisma.$disconnect());
--- test_bcrypt.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-async function test() {
-  const hash = "$2b$10$Epz0oN4hO3X5zY8A8R.5O.Xk7H/qF9OqHkZ1E/E8i2t9h1I9Y4Yq2";
-  const match = await bcrypt.compare('password123', hash);
-  console.log('Match?', match);
-}
-
-test();
